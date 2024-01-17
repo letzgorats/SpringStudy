76290c45ce57bd5b1b0f8e78d865bdea2096c02f
letzgorats <a01030659322@gmail.com>
2024-01-17 19:13:55 +0900

    [feat]AppConfig Refactoring

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -14,17 +16,28 @@ import hello.core.order.OrderServiceImpl;
 // AppConfig 는 외부 기획자라고 생각하면 된다.
 public class AppConfig {
 
+    // memberService 역할
     public MemberService memberService() {
         // 생성자 주입을 통해 객체 지향 완성
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(getMemberRepository());
     }
 
+    // memberRepository 역할
+    private static MemoryMemberRepository getMemberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    // order 서비스 역할
     public OrderService orderService(){
 
-        return new OrderServiceImpl(new MemoryMemberRepository(),new FixDiscountPolicy());
+        return new OrderServiceImpl(getMemberRepository(), discountPolicy());
     }
 
-
-
+    // discountPolicy 역할
+    public DiscountPolicy discountPolicy(){
+//        return new FixDiscountPolicy();
+        // 정액 정책 --> 정률 정책으로 변경
+        return new RateDiscountPolicy();
+    }
 
 }
