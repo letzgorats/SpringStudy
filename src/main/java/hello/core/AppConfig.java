380cfd2302d6aa71828e8d80ab954fa825658a2d
letzgorats <a01030659322@gmail.com>
2024-01-17 21:47:16 +0900

    [feat]Querying the beans registered in the container

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -9,30 +9,38 @@ import hello.core.member.MemoryMemberRepository;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 
 // DIP 위반 문제를 Interface 뿐만 아니라 구현체에도 의존하는 DIP 문제를 해결했는데, 변경했기 때문에 OCP 문제가 생김.
 // 이를 해결하기 위해 AppConfig 클래스 생성 -> 애플리케이션에 대한 환경 구성에 대한 것을 여기에 다 하는 것이다.-> OCP 해결
 // AppConfig 는 외부 기획자라고 생각하면 된다.
+
+@Configuration
 public class AppConfig {
 
+    @Bean
     // memberService 역할
     public MemberService memberService() {
         // 생성자 주입을 통해 객체 지향 완성
         return new MemberServiceImpl(getMemberRepository());
     }
 
+    @Bean
     // memberRepository 역할
     private static MemoryMemberRepository getMemberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     // order 서비스 역할
     public OrderService orderService(){
 
         return new OrderServiceImpl(getMemberRepository(), discountPolicy());
     }
 
+    @Bean
     // discountPolicy 역할
     public DiscountPolicy discountPolicy(){
 //        return new FixDiscountPolicy();
